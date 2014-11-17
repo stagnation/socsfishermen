1ddc7db13cd2ecf842cacbfb92f4abfa9ce9bae1
Nils Wireklint <nils.wireklint@gmail.com>
2014-11-17 16:20:12 +0100

    first draft people

---
+++ people.py
@@ -0,0 +1,25 @@
+class People:
+
+    def __init__(self, x, y, threshhold):
+        self.x = x
+        self.y = y
+        self.threshhold = threshhold
+        self.catch = 0
+
+    def move(self, x, y):
+        self.x = x
+        self.y = y
+
+    def fish():
+        #interaction with the fish / sea ?
+        if self.catch < self.threshold:
+            ...
+
+    def findNewDirection():
+        #interaction with fish / sea ?
+        #sample fish population in a neighborhood
+        #and select a new direction to move in.
+
+        ...
+
+        
