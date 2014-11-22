05a8d5ceb90f36a89b5f8802d3442b73bbeeffb2
Nils Wireklint <nils.wireklint@gmail.com>
2014-11-22 16:43:10 +0100

    added simple time and fish/people grid split

--- sea.py
+++ sea.py
@@ -10,11 +10,13 @@ class Sea:
         self.size = size_tup
         self.grid = [ [ 0  for x in range(size_tup[1]) ] for y in range(size_tup[0]) ]
         self.carrying_capacity = 1 + sp.random.rand(size_tup[0],size_tup[1]);
-        #Set the total carrying capacity to twice the size of initial population (this can be changed) /EÃƒâ€¦
-        self.carrying_capacity = self.carrying_capacity / sum(sum(self.carrying_capacity)) * 2 * population_total
+        #Set the total carrying capacity to twice the size of initial population (this can be changed)
+        self.carrying_capacity = self.carrying_capacity / self.carrying_capacity.sum() * 2 * population_total
         #exchange 0 for empty lsit or data struct for easy add/rem of objects
         self.fish_list = []
         remaining_fish = population_total
+        #this needs some tweaking to make sure there are no mega schools added
+        #with the remaining fish at the end.
         rand_dist  = sp.random.rand(fish_initial_count)
         fish_dist = rand_dist / sum(rand_dist) * population_total
 
@@ -46,26 +48,34 @@ class Sea:
     def print_fish(self):
         mat = self.to_mat()
         print(mat)
+    def grow_fishes(self):
+        for f in self.fish_list:
+            f.grow(sea.carrying_capacity[f.x][f.y])
 
-def move_fishes(sea):
-    #Modified diffusion model to make sure only one fish is on same location
-    for f in sea.fish_list:
-        directions = [(-1,0), (0,-1), (0,1), (0,1)]
-        while True:
-            if len(directions) == 0:
-                pos = (f.x, f.y)
-                break
-            delta = choice(directions)
-            pos = ((f.x + delta[0]) % sea.size[0], (f.y + delta[1]) % sea.size[1])
-            if not isinstance(sea.grid[pos[0]][pos[1]], Fish):
-                break;
-            directions.remove(delta)
+    def fish_population_size(self):
+        popul_size = 0
+        for f in self.fish_list:
+            popul_size += f.count
+        return popul_size
 
-        sea.grid[f.x][f.y] = 0
-        f.x = pos[0]
-        f.y = pos[1]
-        sea.grid[f.x][f.y] = f
 
-def grow_fishes(self):
-    for f in self.fish_list:
-        f.grow(sea.carrying_capacity[f.x][f.y])
+    def move_fishes(self):
+        #Modified diffusion model to make sure only one fish is on same location
+        for f in self.fish_list:
+            directions = [(-1,0), (0,-1), (0,1), (1,0)]
+            while True:
+                if len(directions) == 0:
+                    pos = (f.x, f.y)
+                    break
+                delta = choice(directions)
+                pos = ((f.x + delta[0]) % self.size[0], (f.y + delta[1]) % self.size[1])
+                if not isinstance(self.grid[pos[0]][pos[1]], Fish):
+                    break;
+                directions.remove(delta)
+
+            self.grid[f.x][f.y] = 0
+            f.x = pos[0]
+            f.y = pos[1]
+            self.grid[f.x][f.y] = f
+
+##########END OF CLASS
--- utils.py
+++ utils.py
@@ -6,3 +6,20 @@ def discrete_trajectory(vector, allowed_directions = [(-1,0), (0,-1), (0,1), (1,
     dot_products = [ sp.dot( vector, d)  for d in allowed_directions ]
     max_idx = dot_products.index(max(dot_products))
     return allowed_directions[max_idx]
+
+def neighborhood_tuples(x,y,side_length):
+    #from -floor(side_lngth / 2) to this plus side_length
+    tup_list = []
+    #x_start = x - sp.floor(side_length / 2)
+    #y_start = y - sp.floor(side_length / 2)
+    for i in range(int(side_length)):
+        for j in range(int(side_length)):
+            tup_list.append( (int(x + i), int(y + j) ) )
+    return tup_list
+
+if __name__ == '__main__':
+    #test neighbordhood_tuples:
+    x = 10
+    y = 10
+    side_length = 3
+    print(neighborhood_tuples(x,y,side_length))
